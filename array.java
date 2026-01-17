d4002860fac246d047d570562eaae4673e5469a8
Ridhi585 <ridhitrehan585@gmail.com>
2026-01-17 23:26:06 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,26 @@
+ import java.io.*;
+import java.util.*;
+    public class array {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int[][] arr = new int[6][6];
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            int maxSum = Integer.MIN_VALUE;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    int currentSum =
+                            arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                                    + arr[i + 1][j + 1]
+                                    + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                    maxSum = Math.max(maxSum, currentSum);
+                }
+            }
+            System.out.println(maxSum);
+            sc.close();
+        }
+    }
